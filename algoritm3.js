31ec79ee3c4df232b10d6b3d34a3d171a6518caa
Добродеев Валера <33656818+Dobrodeev@users.noreply.github.com>
2022-11-18 21:44:41 +0300

    скрипт который открывает и читает файлю

---
+++ algoritm3.js
@@ -0,0 +1,44 @@
+/**
+ * переша структурна единица - рядок
+ * создать большой обьект и заганяй туда маленькие обьекты 
+ * массив обьектов "", "", "" 200 рядкив и кожен рудок - масив 5 элементов
+ */
+ var fs = require('fs');
+ const { parse } = require('querystring');
+ fs.open("file.txt", 'r', function(err, fd){
+     if(!err){
+         console.log('file is opening...');
+         var options = {encoding:'utf8', flag:'r'};
+         fs.readFile('file.txt', options, function(err, data){
+             if(err){
+                 console.log("Failed to open config file.");
+             }else{
+                 console.log("Config Loaded.");
+                 // Entire file
+             //console.log(this.result);
+             // By lines
+             // var n = 10;
+             // console.log(data);
+             
+             var lines = data.split('\n');
+             console.log('There are %d lines.', lines.length);
+             console.log(data);
+            //  console.log(lines);
+             
+             for(let i = 0; i < lines.length; i ++){            
+             
+             }
+            
+             }
+             
+         })
+         
+         fs.close(fd, function(err){
+             if(err){
+                 console.log(err);
+             }
+             console.log("File closed successfully");
+         });        
+     }    
+ });
+ 
